4117dc2cbcf40a0a6e77d628a48e2a7a2d5693c4
joaquinbowen <bowenjoaquinsd@gmail.com>
2025-06-24 08:02:50 -0500

    consultass

--- consultasRelacionales/consultaEstudiantes.sql
+++ consultasRelacionales/consultaEstudiantes.sql
@@ -1,6 +1,7 @@
 select pro.codigo, est.nombre, est.apellido
-from estudiantes est,profesores pro
-where est.nombre like '%n%' and est.apellido like '%n%';
+from estudiantes est, profesores pro
+where est.codigo_profesor = pro.codigo 
+and est.apellido like '%n%';
 
 select est.* from estudiantes est,profesores prof
 where prof.nombre = 'Francisco' and est.codigo_profesor = 1;
